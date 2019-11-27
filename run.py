596ce141954a463ce86fac1b2c96d5b446cf524f
Michaela Ockova <ockova.michaela@gmail.com>
2019-11-27 20:56:13 +0100

    Add formatting

--- run.py
+++ run.py
@@ -9,6 +9,11 @@ import openpyxl as openpyxl
 import requests
 
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
+from openpyxl.formatting import Rule
+from openpyxl.styles import PatternFill, Alignment
+from openpyxl.styles.differential import DifferentialStyle
+from openpyxl.styles.numbers import FORMAT_PERCENTAGE_00
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -78,14 +83,18 @@ def parse_table(webpage: str) -> list:
     return stock_predictions
 
 
-def _get_performance_prediction(soup: BeautifulSoup, column_number: int) -> dict:
-    return soup.find(
+def _get_performance_prediction(soup: BeautifulSoup, column_number: int) -> float:
+    prediction = soup.find(
         "td",
         attrs={
             "class": "table-cell-label kv-align-right kv-align-middle w3",
             "data-col-seq": column_number,
         },
     ).get_text()
+
+    clean_prediction = prediction.replace(" ", "").strip("%")
+
+    return float(clean_prediction) / 100
 
 
 def add_stock_identification(stock_identifiers: tuple, stock_data: list) -> list:
@@ -94,14 +103,16 @@ def add_stock_identification(stock_identifiers: tuple, stock_data: list) -> list
     return stock_row
 
 
-def write_to_excel(source_file: str, file_to_write: str, predictions: List[list]):
+def write_to_excel(
+    source_file: str, file_to_write: str, predictions: List[list], sheetname: str
+) -> Workbook:
     if Path(file_to_write).exists():
         wb = openpyxl.load_workbook(file_to_write)
     else:
         copyfile(source_file, file_to_write)
         wb = openpyxl.load_workbook(file_to_write)
 
-    ws = wb.create_sheet(title=date.today().strftime("%d-%m-%Y"))
+    ws = wb.create_sheet(title=sheetname)
 
     column_headers = [
         "Company",
@@ -122,8 +133,40 @@ def write_to_excel(source_file: str, file_to_write: str, predictions: List[list]
 
     wb.save(file_to_write)
 
+    return wb
 
-def get_stock_predictions(file_with_stocks: str, file_with_predictions: str):
+
+def apply_formatting(output_file: str, sheetname: str) -> None:
+    wb = openpyxl.load_workbook(output_file)
+    ws = wb[sheetname]
+
+    # Change size of the column with company names and wrap the text
+    ws.column_dimensions["A"].width = 25
+    for cell in ws["A"]:
+        cell.alignment = Alignment(wrap_text=True)
+
+    # Add column filters
+    ws.auto_filter.ref = ws.dimensions
+
+    # Change format of predictions from string to percentage
+    percentage_columns = ["E", "F", "G", "H", "I"]
+    for col in percentage_columns:
+        for cell in ws[col]:
+            cell.number_format = FORMAT_PERCENTAGE_00
+
+    # Add conditional formatting for predictions percentages
+    green_background = PatternFill(bgColor="C6EFCE")
+    diff_style = DifferentialStyle(fill=green_background)
+    rule = Rule(type="cellIs", operator="greaterThan", dxf=diff_style, formula=["30.00%"])
+    ws.conditional_formatting.add(f"F2:G{ws.max_row}", rule)
+
+    # Hide the column with 5 year predictions
+    ws.column_dimensions["I"].hidden = True
+
+    wb.save(output_file)
+
+
+def get_stock_predictions(file_with_stocks: str) -> list:
     predictions = []
     stocks_to_predict = get_stock_identifiers(file_with_stocks)
 
@@ -135,9 +178,11 @@ def get_stock_predictions(file_with_stocks: str, file_with_predictions: str):
             continue
         stock_row = add_stock_identification(stock, parsed_stock_values)
         predictions.append(stock_row)
-        time.sleep(5)
+        time.sleep(1)
+
     sorted(predictions, key=lambda x: x[1])
-    write_to_excel(file_with_stocks, file_with_predictions, predictions)
+
+    return predictions
 
 
 if __name__ == "__main__":
@@ -145,10 +190,15 @@ if __name__ == "__main__":
 
     file_with_stocks = "data/revolut_stocks.xlsx"
     file_with_predictions = "data/predictions.xlsx"
+    sheet_name = date.today().strftime("%d-%m-%Y")
 
     old_predictions = check_latest_predictions(file_with_predictions)
 
     if old_predictions:
-        get_stock_predictions(file_with_stocks, file_with_predictions)
+        stock_predictions = get_stock_predictions(file_with_stocks)
+        output_workbook = write_to_excel(
+            file_with_stocks, file_with_predictions, stock_predictions, sheet_name
+        )
+        apply_formatting(file_with_predictions, sheet_name)
 
     logging.info("...scraping finished")
