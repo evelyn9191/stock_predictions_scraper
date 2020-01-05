2a9366656a6320f966dcd6e56dcbe611323e5365
Michaela Ockova <ockova.michaela@gmail.com>
2020-01-05 13:14:15 +0100

    Parse consensus

--- run.py
+++ run.py
@@ -14,6 +14,11 @@ from openpyxl.formatting import Rule
 from openpyxl.styles import PatternFill, Alignment
 from openpyxl.styles.differential import DifferentialStyle
 from openpyxl.styles.numbers import FORMAT_PERCENTAGE_00
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -129,7 +134,6 @@ def write_to_excel(
 
     for one_stock in predictions:
         ws.append(one_stock)
-        logging.info(f"Added: {one_stock}")
 
     wb.save(file_to_write)
 
@@ -185,6 +189,75 @@ def get_stock_predictions(file_with_stocks: str) -> list:
     return predictions
 
 
+def initialize_driver() -> WebDriver:
+    chrome_options = Options()
+    chrome_options.add_argument(
+        "user-agent=[Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36]"
+    )
+    driver = webdriver.Chrome(".\\chromedrivers\\win.exe", chrome_options=chrome_options)
+    return driver
+
+
+def get_tickers(file_with_stocks: str) -> list:
+    wb = openpyxl.load_workbook(file_with_stocks)
+    ws = wb.active
+
+    tickers = []
+    for row in ws.iter_rows(min_row=2, values_only=True):
+        tickers.append(row[1])
+
+    logging.debug(tickers)
+    return tickers
+
+
+def get_consensus(driver: WebDriver, ticker: str) -> str:
+    driver.get(f"https://www.nasdaq.com/market-activity/stocks/{ticker}/analyst-research")
+    consensus_xpath = (
+        "/html/body/div[4]/div/main/div/div[4]/div[2]/div/div[1]/div[2]/div[2]/div/span[2]"
+    )
+    ActionChains(driver).move_to_element(driver.find_element_by_xpath(consensus_xpath)).perform()
+    consensus = driver.find_element_by_xpath(consensus_xpath).text
+    print(f"{ticker + ': ' + consensus}")
+    return consensus
+
+
+def add_consensus_column(file_with_predictions: str, sheetname: str, consensuses: list) -> None:
+    wb = openpyxl.load_workbook(file_with_predictions)
+    ws = wb[sheetname]
+
+    consensuses.insert(0, ["J1", "Consensus"])
+
+    for consensus in consensuses:
+        cell = ws[consensus[0]]
+        cell.value = consensus[1]
+
+    wb.save(file_with_predictions)
+
+
+def add_consensus(file_with_predictions: str, sheet_name: str) -> None:
+    driver = initialize_driver()
+    tickers = get_tickers(file_with_predictions)
+    ticker_consensuses = []
+
+    logging.info("Scraping consensuses...")
+    current_index = 2
+    unknown_consensus = "N/A"
+    for ticker in tickers:
+        try:
+            analysts_consensus = get_consensus(driver, ticker)
+        except NoSuchElementException:
+            analysts_consensus = unknown_consensus
+        ticker_consensuses.append(
+            [f"J{current_index}", analysts_consensus.strip() or unknown_consensus]
+        )
+        current_index += 1
+        time.sleep(1)
+    logging.info("...finished scraping consensuses.")
+    driver.close()
+
+    add_consensus_column(file_with_predictions, sheet_name, ticker_consensuses)
+
+
 if __name__ == "__main__":
     logging.info("Running scraper...")
 
@@ -196,9 +269,8 @@ if __name__ == "__main__":
 
     if old_predictions:
         stock_predictions = get_stock_predictions(file_with_stocks)
-        output_workbook = write_to_excel(
-            file_with_stocks, file_with_predictions, stock_predictions, sheet_name
-        )
+        write_to_excel(file_with_stocks, file_with_predictions, stock_predictions, sheet_name)
+        add_consensus(file_with_predictions, sheet_name)
         apply_formatting(file_with_predictions, sheet_name)
 
     logging.info("...scraping finished")
